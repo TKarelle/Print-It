db9d884f015b54e1a146590e352a8bfd37d68087
Karelle table <karelle.tble@gmail.com>
2024-11-22 11:21:50 +0000

    Initial commit

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,62 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.getElementById('arrow_left');
+const arrowRight = document.getElementById('arrow_right');
+const banner = document.querySelector('.banner-img'); 
+const tagline = document.querySelector('.banner-p');
+let currentIndex = 0;
+
+arrowLeft.addEventListener('click', () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1; 
+    }
+    updateBanner();
+});
+
+arrowRight.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0; 
+    }
+    updateBanner();
+});
+
+const dotsContainer = document.querySelector('.dots');
+slides.forEach ((slides,index) => {
+	const dot = document.createElement('span')
+    dot.classList.add('dot')
+	dot.setAttribute('data-index', index); 
+    dotsContainer.appendChild(dot); 
+});
+
+function updateDots(){
+	const dots = document.querySelectorAll('.dot');
+	dots.forEach (dot => dot.classList.remove('dot_selected'));
+	dots[currentIndex].classList.add('dot_selected');
+}
+
+function updateBanner() {
+    banner.src = slides[currentIndex].image;
+	tagline.innerHTML = slides[currentIndex].tagLine;
+	updateDots();
+}
+
+updateBanner();
